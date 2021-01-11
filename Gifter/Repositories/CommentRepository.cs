553702a1f605d0cdb89048c9f3c9f9315e3d1710
Joe <overtonje@gmail.com>
2021-01-11 15:59:36 -0600

    working on comment repository

---
+++ Gifter/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using Gifter.Data;
+using Gifter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Repositories
+{
+    public class CommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Comment> GetAll()
+        {
+            return _context.Comment
+                .Include(c => c.UserProfile)
+                .Include(c => c.Post)
+                .ToList();
+        }
+
+        public List<Comment> GetCommentsByPostId(int id)
+        {
+            return _context.Comment
+                .Include(c => c.UserProfile)
+                .Include(c => c.Post)
+                .Where(c => c.PostId == id)
+                .ToList();
+        }
+
+        public Comment 
+
+    }
+}
