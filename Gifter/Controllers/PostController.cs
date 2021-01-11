643050175efe7b356caed9747d924ffbe6d7b53c
Joe <overtonje@gmail.com>
2021-01-11 14:47:25 -0600

    post crud complete

---
+++ Gifter/Controllers/PostController.cs
@@ -0,0 +1,75 @@
+﻿using Gifter.Models;
+using Gifter.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gifter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+        public PostController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_postRepository.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var post = _postRepository.GetById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return Ok(post);
+
+        }
+
+        [HttpGet("getbyuser/{id}")]
+        public IActionResult GetByUserProfileId(int id)
+        {
+            return Ok(_postRepository.GetByUserProfileId(id));
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Post post)
+        {
+            _postRepository.Add(post);
+            return CreatedAtAction("Get", new { id = post.Id }, post);
+
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Post post)
+        {
+            if (id != post.Id)
+            {
+                return BadRequest();
+            }
+
+            _postRepository.Update(post);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _postRepository.Delete(id);
+            return NoContent();
+        }
+
+
+
+    }
+}
