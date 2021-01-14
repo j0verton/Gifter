0ec592ec4475193cb9e815c110e3b3fb74c73130
Joe <overtonje@gmail.com>
2021-01-14 12:05:04 -0600

    currently not compiling was working on form, broke for reading

--- Gifter/client/src/components/PostForm.js
+++ Gifter/client/src/components/PostForm.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { FormGroup, Form, Label, Input } from 'reactstrap'
+
 const PostForm = () => {
+
+    useContext(PostList)
     return (
         <div className="container">
             <div className="row justify-content-center">
@@ -8,7 +11,27 @@ const PostForm = () => {
                     <FormGroup>
                         <Label for="title">Title
                         </Label>
-                        <Input type="text" name="title" id="">
+                        <Input type="text" name="title" id="">Post Title
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="imageUrl">Add a Gif Link
+                        </Label>
+                        <Input type="text" name="imageUrl" id="">https://media.giphy.com/media/XZrOvaUvmsCYL31HIe/giphy.gif
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="caption">Caption
+                        </Label>
+                        <Input type="text" name="caption" id="">Post Title
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="hidden" name="UserProfileId" id="">Post Title
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Input type="hidden" name="dateCreated" id="">Post Title
                         </Input>
                     </FormGroup>
 
@@ -16,7 +39,6 @@ const PostForm = () => {
 
             </div>
         </div>
-
     )
-
 }
+export default PostForm;
