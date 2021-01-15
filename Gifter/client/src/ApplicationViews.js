521ce96bfb612d80418957e38d4a39650cd6a2b0
Joe <overtonje@gmail.com>
2021-01-15 10:45:32 -0600

    applicaiton views

--- Gifter/client/src/ApplicationViews.js
+++ Gifter/client/src/ApplicationViews.js
@@ -1,8 +1,30 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import PostList from "./PostList";
+import PostForm from "./PostForm";
 
-// <Route path="/login">
-//     <Login />
-// </Route>
+const ApplicationViews = () => {
+    return (
+        <Switch>
+            <Route path="/login">
+                <Login />
+            </Route>
 
-// <Route path="/register">
-//     <Register />
-// </Route>
+            <Route path="/register">
+                <Register />
+            </Route>
+
+            <Route path="/" exact>
+                <PostList />
+            </Route>
+
+            <Route path="/posts/add">
+                <PostForm />
+            </Route>
+
+            <Route path="/posts/:id">{/* TODO: Post Details Component */}</Route>
+        </Switch>
+    );
+};
+
+export default ApplicationViews;
